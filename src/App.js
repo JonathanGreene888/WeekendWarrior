f1eb8f0cf45683fd985f32352f307c05c72d5c21
jonathan <jonathangreene888@gmail.com>
2021-06-04 23:56:28 -0700

    Added some context for the basics of what the site will center on

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
         <h1>
           WeekendWarriorConsulting
         </h1>
+        <h3> Name your price! $$$ </h3>
+        <h3> Choose your developer! </h3>
+        <h3> React based. Clean, Current and Custom. </h3>
+        <h3> All pay goes to developer and developer training.</h3>
+        <h3> Help an entry level developer level up while being assured they are overseen and all code is checked by more senior peers. </h3>
       </header>
     </div>
   );
