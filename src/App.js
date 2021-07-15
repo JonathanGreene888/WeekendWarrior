aa014e79981775aa2ff8421307fac715ca7e49f7
Jonathan Greene <Jonathangreene888@gmail.com>
2021-07-14 23:08:25 -0700

    updated base code to match more of the vision

--- src/App.js
+++ src/App.js
@@ -7,23 +7,14 @@ function App() {
         <h1>
           WeekendWarriorConsulting
         </h1>
-        <h3> Name your price! $$$ </h3>
-        <div>
-         $20 <input type="range" min="20" max="5000" class="slider"/> $5000
-        </div>
-        <h3> Choose your developer! </h3>
-        <button> Choose here! </button>
-        <h3> Choose if you provide benefits! </h3>
-        <form>
-        <label for="Yes">Yes</label>
-          <input type="radio" id="Yes" name="benefits" value="Yes"/>
-          <label for="No">No</label>
-          <input type="radio" id="No" name="benefits" value="No"/>
-        </form>
-        <h3> React based. Clean, Current and Custom. </h3>
-        <h3> All pay goes to developer and developer training.</h3>
-        <h3> Help an entry level developer level up while being assured they are overseen and all code is checked by more senior peers. </h3>
       </header>
+        <div className='Border-box'>
+        <h3> High Quality </h3>
+        <div>
+        </div>
+        <h3> React based. Clean, Current and Custom. </h3>
+        <h3> Contact us at jonathangreene888@gmail.com. </h3>
+        </div>
     </div>
   );
 }
